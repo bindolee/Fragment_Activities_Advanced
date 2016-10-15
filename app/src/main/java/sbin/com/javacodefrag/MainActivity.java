58d10e70352ca1cd15ed1caee2cb87ca57c735fe
bindolee <bindolee@gmail.com>
2016-10-14 22:14:35 -0700

    Added function to display fragment + framelayout for tablet and for phone, go to use Intent.

--- app/src/main/java/sbin/com/javacodefrag/MainActivity.java
+++ app/src/main/java/sbin/com/javacodefrag/MainActivity.java
@@ -1,14 +1,19 @@
 package sbin.com.javacodefrag;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean mTablet;
+    private ViewGroup fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +28,43 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tvOut = (TextView) findViewById(R.id.textOut);
         tvOut.setText("Fragment side-by-side? "+ mTablet);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {viewDetailFragment();
+            }
+        });*/
     }
 
+    public void viewDetailFragment(){
+        if (mTablet){
+            FragmentManager fragmentManager =
+                    getSupportFragmentManager();
+            DetailActivityFragment fragment = new DetailActivityFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.detail_fragment_container, fragment)
+                    .commit();
+        }
+        else{
+            Intent intent = new Intent(this, DetailActivity.class);
+            startActivity(intent);
+        }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings)
+            viewDetailFragment();
+
+        return true;
+    }
 }
